9af3e9e696418b41e45275ae455ca0b837a3c824
Rafaela Moreira <45953979+rafapcmor@users.noreply.github.com>
2024-11-13 09:48:35 -0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,1 +1,14 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe seu nome:");
+        String name = scanner.nextLine();
+        System.out.println("Olá, " + name + "!");
+        scanner.close();
+    }
+}
 
